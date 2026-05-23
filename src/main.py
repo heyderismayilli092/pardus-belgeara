3243d26024a19010ceb0feb41185a2626b746f55
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-05-23 14:41:28 +0400

    The  function has been improved and functions for buttons have been added

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, GObject, Gdk
 
 import os
 import locale
@@ -33,6 +33,7 @@ class pardusdocsearch:
         # Main Window
         self.mainwindow.connect("destroy", self._on_destroy)
         self.mainwindow.show_all()
+        self.cssload()
 
         for f in files_list():
             row = self.create_row(os.path.basename(f), f)
@@ -41,30 +42,80 @@ class pardusdocsearch:
 
 
 
+
+    # CSS theme
+    def cssload(self):
+        css = b"""
+        #listbox {
+          margin-bottom: 20px;
+        }
+        """
+        provider = Gtk.CssProvider()
+        provider.load_from_data(css)
+        screen = Gdk.Screen.get_default()
+        Gtk.StyleContext.add_provider_for_screen(
+            screen,
+            provider,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+        )
+
+
     # row function to be created for each data point
     def create_row(self, filename, fullpath):
-        # Main Box
-        row_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
 
-        image = Gtk.Image.new_from_icon_name("text-x-generic", Gtk.IconSize.BUTTON)  # icon (left)
+        row_box = Gtk.Box(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            spacing=5
+        )
 
-        label = Gtk.Label(label=filename)  # filename label (center)
-        label.set_xalign(0)  # align left
+        # ICON box
+        image = Gtk.Image.new_from_icon_name("text-x-generic",Gtk.IconSize.BUTTON)
+        image.set_halign(Gtk.Align.START)
 
-        # Right: vertical spiral buttons
-        button_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        # TEXT box
+        text_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        text_box.set_halign(Gtk.Align.CENTER)
+
+        label_name = Gtk.Label(label=filename)
+        label_name.set_xalign(0)
+
+        # the file path text is being shortened
+        short_path = fullpath[:15] + "..." if len(fullpath) > 15 else fullpath
+        label_path = Gtk.Label(label=short_path)
+        label_path.set_xalign(0)
+
+        text_box.pack_start(label_name, False, False, 0)
+        text_box.pack_start(label_path, False, False, 0)
+
+        # BUTTON box
+        button_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        button_box.set_halign(Gtk.Align.END)
+
         button_open = Gtk.Button(label="Open")
         button_info = Gtk.Button(label="Info")
 
-        # Add buttons
+        # -------Signals-------
+        button_open.connect("clicked", self.on_open_clicked, fullpath)
+        button_info.connect("clicked", self.on_info_clicked, fullpath)
+
         button_box.pack_start(button_open, False, False, 0)
         button_box.pack_start(button_info, False, False, 0)
-        # Row layout
-        row_box.pack_start(image, False, False, 5)
-        row_box.pack_start(label, True, True, 5)
-        row_box.pack_end(button_box, False, False, 5)
+
+        # ROW placement (efforts were made to minimize gaps)
+        row_box.pack_start(image, False, False, 3)
+        row_box.pack_start(text_box, False, False, 3)
+        row_box.pack_end(button_box, False, False, 3)
 
         return row_box
+
+
+    # test
+    def on_open_clicked(self, button, fullpath):
+        print("OPEN:", fullpath)
+
+    # test
+    def on_info_clicked(self, button, fullpath):
+        print("INFO:", fullpath)
 
 
     def _on_destroy(self, widget):
